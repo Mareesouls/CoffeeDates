eaa845a029c31489ba957fb28eb1b1558b9adf16
BanditZois <1026086@student.sae.edu.au>
2023-02-19 20:08:17 +1100

    Scene change added to buttons (except menu)

---
+++ Assets/Scripts/Scene_Change/SC_Barista.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SC_Barista : MonoBehaviour
+{
+    public void SCBarista()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
