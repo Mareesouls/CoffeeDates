06e3f45aff679cc84adb13247c48148fb17512f6
Xavier <67317037+Thekoa@users.noreply.github.com>
2023-02-20 12:44:55 +1100

    Button functions complete

--- Assets/Scripts/Mechanics/CM_Maker/CoffeeMachine.cs
+++ Assets/Scripts/Mechanics/CM_Maker/CoffeeMachine.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CoffeeMachine : MonoBehaviour
 {
-    public int water = 0;
-    public int sugar = 0;
+    int water = 0;
+    int sugar = 0;
     public int coffee = 0;
-    public int milk = 0;
-
+    int milk = 0;
+    int maxIngreadients = 6;
     void CoffeeComplete()
     {
     /*Count all values and check what dink is made, if the drink does not match anything the list has to offer, then it pumps out a weird drink.*/
@@ -16,9 +16,61 @@ public class CoffeeMachine : MonoBehaviour
     /*Print name of drink*/
     }
 
+
+    //button functions
     public void CoffeeButton()
     {
-        print("Coffee is added");
+        if (coffee<maxIngreadients)
+        {
+            print("Coffee is added");
+            coffee++;
+        }
+        else
+        {
+            print("You can not add anymore!!!");
+        }
+    }
+
+    public void SugarButton()
+    {
+         if (sugar<maxIngreadients)
+        {
+            print("sugar is added");
+            sugar++;
+        }
+        else
+        {
+            print("You can not add anymore!!!");
+        }
+    }
+
+    public void WaterButton()
+    {
+         if (water<maxIngreadients)
+        {
+            print("Water is added");
+            water++;
+        }
+        else
+        {
+            print("You can not add anymore!!!");
+        }
+
+    }
+
+    public void MilkButton()
+    {
+        print("Milk is added");
+        milk++;
+    }
+
+    public void Reset()
+    {
+        print("Reset!");
+        sugar = 0;
+        milk = 0;
+        water = 0;
+        coffee = 0;
     }
 
 
