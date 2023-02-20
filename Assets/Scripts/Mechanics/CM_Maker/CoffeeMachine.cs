f24346e69c3ae9eec7e815e27761abcb7055e469
Xavier <67317037+Thekoa@users.noreply.github.com>
2023-02-20 12:03:39 +1100

    Started working on the Coffee Manchine

---
+++ Assets/Scripts/Mechanics/CM_Maker/CoffeeMachine.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeMachine : MonoBehaviour
+{
+    public int water = 0;
+    public int sugar = 0;
+    public int coffee = 0;
+    public int milk = 0;
+
+    void CoffeeComplete()
+    {
+    /*Count all values and check what dink is made, if the drink does not match anything the list has to offer, then it pumps out a weird drink.*/
+    /*Sets all int to 0*/
+    /*Print name of drink*/
+    }
+
+    public void CoffeeButton()
+    {
+        print("Coffee is added");
+    }
+
+
+}
