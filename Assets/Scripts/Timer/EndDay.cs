ca6a109c1b852636d1fd32119165b5492b59ef7f
BanditZois <1026086@student.sae.edu.au>
2023-02-21 21:18:11 +1100

    Working on timer again

---
+++ Assets/Scripts/Timer/EndDay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDay : MonoBehaviour
+{
+	
+	DayTimer customerGone;
+	
+    // Start is called before the first frame update
+    void ButtonClicked()
+    {
+		if(customerGone = false)
+		{
+			print("You still have a customer!");
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
