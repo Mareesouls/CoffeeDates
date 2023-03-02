239bb62390d8d9d8c80983c6fa80721b6ebf96c8
Mareesouls <99461205+Mareesouls@users.noreply.github.com>
2023-03-02 12:28:36 +1100

    added font

--- Assets/Scripts/Customer_Script/DialogueManager.cs
+++ Assets/Scripts/Customer_Script/DialogueManager.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> sentences;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +22,37 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
-        Debug.Log("Starting Conversation With" + dialogue.name);
+     Debug.Log("Starting Conversation With" + dialogue.name);
+
+       // animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count > 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+      //  animator.SetBool("IsOpen", false);
+        Debug.Log("End of Conversation.");
     }
 
    
