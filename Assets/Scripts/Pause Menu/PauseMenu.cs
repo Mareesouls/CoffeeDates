e2ae17ed05813cee6d12364f594003ce3476b0f6
Kelly <1020117@student.sae.edu.au>
2023-03-08 13:03:44 +1100

    Updated PauseMenu
    new menu code

--- Assets/Scripts/Pause Menu/PauseMenu.cs
+++ Assets/Scripts/Pause Menu/PauseMenu.cs
@@ -2,68 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour 
-public class static bool GameIsPaused = false;
-
+public class PauseMenu : MonoBehaviour
 {
-void Update()
-{
-    if (Input.GetKeyDown(KeyCode.Escape))
+    // Start is called before the first frame update
+    void Start()
     {
-        if (GameIsPaused)
-        {
-            Resume();
-        }
-        else
-        {
-            Pause();
-        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
-void Resume()
-{
-
-}
-void Pause()
-{
-pauseMenuUI.SetActive(true);
-Time.timeScale=0f;
-}
-}
-
-// {
-// public static bool GameIsPaused = false;
-// //public PauseMenu menuUI;
-
-// PauseMenu
-//     // Update is called once per frame
-//     void Update() 
-//     {
-//         if (Input.GetKeyDown(KeyCode.Escape))
-//         {
-//             if (GameIsPaused==true)
-//             {
-//                 Resume();
-//             } 
-//             else
-//             {
-//                 Pause();
-//             }   
-//         }
-        
-//     }
-//     void Resume ()
-//     {
-//         PauseMenuUI.SetActive(false);
-//         Time.timeScale = 1f;
-//         GameIsPaused = false;
-//     }
-
-//     void Pause ()
-//     {
-//         pauseMenuUI.SetActive(true);
-//         Time.timeScale = 0f;
-//         GameIsPaused = true;
-//     }
-    
-// }
