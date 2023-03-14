bcf2a8e51572bd19c5e19ac2cf962c0c460697e9
Xavier <67317037+Thekoa@users.noreply.github.com>
2023-03-14 21:34:21 +1100

    Adding pause menu and fixing scene change issues

--- Assets/Scripts/Mechanics/CM_Maker/Temp_CoffeeSelect.cs
+++ Assets/Scripts/Mechanics/CM_Maker/Temp_CoffeeSelect.cs
@@ -7,7 +7,7 @@ public class Temp_CoffeeSelect : MonoBehaviour
 {
     public TippingMachine tippingMachine;
     public TMP_Text dialogueText;
-    public TMP_Text RequestText;
+    public TMP_Text requestText;
     string customerDrink;
 
     // Start is called before the first frame update
@@ -21,7 +21,31 @@ public class Temp_CoffeeSelect : MonoBehaviour
     {
         if (tippingMachine.customerRequest>0) 
         {
-            
+            if (tippingMachine.customerRequest == 1) 
+            {
+                dialogueText.text = "I would like to order an Espresso";
+                requestText.text = "Coffee Requested: Espresso";
+            }
+            else if(tippingMachine.customerRequest == 2)
+            {
+                dialogueText.text = "I would like to order a Doppio";
+                requestText.text = "Coffee Requested:  Doppio";
+            }
+            else if(tippingMachine.customerRequest == 3)
+            {
+                dialogueText.text = "I would like to order an Americano";
+                requestText.text = "Coffee Requested: Americano";
+            }
+            else if(tippingMachine.customerRequest == 4)
+            {
+                dialogueText.text = "I would like to order a Latte";
+                requestText.text = "Coffee Requested: Latte";
+            }
+            else if(tippingMachine.customerRequest == 5)
+            {
+                dialogueText.text = "I would like to order a Flat White";
+                requestText.text = "Coffee Requested: Flat White";
+            }
         }
     }
 }
