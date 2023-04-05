1281dfe2ab14fe63f3e391d857cb4bc22594e47d
Kelly <1020117@student.sae.edu.au>
2023-04-05 12:55:42 +1000

    Dialogue coding
    Adding the code for the dialogue with choices

--- Assets/Scripts/Dating Dialogue/NewBehaviourScript.cs
+++ Assets/Scripts/Dating Dialogue/NewBehaviourScript.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class DialogueManager : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start () {
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue (Dialogue dialogue)
     {
-        
+        Debug.Log ("Starting conversation with" + dialogue.name);
+
+        sentences.Clear();
+
+        forench (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
     }
+    
+    public void DisplayNextSentence ()
+    {
+        if (sentence.Count == 0)
+        }
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentence.Dequeue();
+        Debug.Log(sentence);
+
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+
 }
