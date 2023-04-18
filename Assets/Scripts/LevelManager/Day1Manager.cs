8de7cc25006630d0afa9a86b4c24c2396c7a6a15
Xavier <67317037+Thekoa@users.noreply.github.com>
2023-04-18 21:26:17 +1000

    Working on Charlie and UI

--- Assets/Scripts/LevelManager/Day1Manager.cs
+++ Assets/Scripts/LevelManager/Day1Manager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Day1Manager : MonoBehaviour
 {
+    /*Use this script as a template to guide you through the next few days,
+    What we need in the level manager is to have it store is the amount of customer in a day and what datable customer would appear
+    At first this script might seem useless but it is vital to have a basic level manager if we want to reload this level or expand for future updates
+     */
+
+
     // Start is called before the first frame update
     void Start()
     {
