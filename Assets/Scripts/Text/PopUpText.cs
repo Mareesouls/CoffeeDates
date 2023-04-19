d514543576cf28b0bdcf736a8eff73ad0d30fbc6
Xavier <67317037+Thekoa@users.noreply.github.com>
2023-04-19 13:03:53 +1000

    Updating codes for Playtest

--- Assets/Scripts/Text/PopUpText.cs
+++ Assets/Scripts/Text/PopUpText.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PopUpText : MonoBehaviour
 {   
-    /*public GameObject popUp;
+    public GameObject popUp;
     int fadeOutTime;
     int waitTime;
 
     //fades out after 2 seconds upon active
     void OnEnable()
     {
-        waitTime = 3;
+        waitTime = 2;
         //fadeOutTime = 2;
         StartCoroutine(PopUpTime());
     }
@@ -23,6 +23,7 @@ public class PopUpText : MonoBehaviour
             yield return new WaitForSeconds(waitTime);
             //FadeOut();
             popUp.SetActive(false);
+            print("Pop up disabled");
         }
         yield break;
     }
